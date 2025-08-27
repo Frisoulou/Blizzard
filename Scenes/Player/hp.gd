e431aa503e36d7d670768855befb5db9af238965
Frisoulou <al.ecrement7@gmail.com>
2025-08-28 00:55:57 +0200

    27.08.25 AE modif sprite house + test HP
    Modif sprite house, test HP bar + script, color test = black + fix ordering

---
+++ Scenes/Player/hp.gd
@@ -0,0 +1,21 @@
+extends Node
+@export var wolf : CharacterBody2D
+
+var health: int = 3
+var max_health: int = 100
+
+func _input(event: InputEvent) -> void:
+ 
+#Put a if here
+
+		# Reduce health
+		health = max(health - 1, 0)  # Ensures it doesn't go below 0
+		print("Health:", health)
+
+		# Check conditions
+		if health <= 0:
+			print("You died")
+		elif health < max_health / 2:
+			print("bof")
+		else:
+			print("ok")
