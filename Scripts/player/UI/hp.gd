3495109d4d8e4dbd730561bfe20e70a881b491c6
Frisoulou <al.ecrement7@gmail.com>
2025-08-30 19:42:27 +0200

    AE 30.08.25

--- Scripts/player/UI/hp.gd
+++ Scripts/player/UI/hp.gd
@@ -4,18 +4,19 @@ extends Node
 @onready var hp_bar: Label = $"../Player_UI/HP_bar"
 @onready var game_over: CanvasLayer = $"../Game_over"
 
+
 var health: int = 3
 var max_health: int = 3
 
-#func _process(delta: float) -> void:
-	#if animated_sprite_2d.animation == "Attacking":
-		#health = max(health -1, 0)
-		#hp_bar.text = str(health, "/", max_health)
-	#
-	#if health == 0:
-		#print("hello")
-		#get_node("../Game_over_screen").game_over()
+func _process(delta: float) -> void:
+	if animated_sprite_2d.animation == "Attacking":
+		health = max(health -1, 0)
+		hp_bar.text = str(health, "/", max_health)
 	
+	if health == 0:
+		print("hello")
+		get_node("../Game_over_screen").game_over()
+
 
 		## Reduce health
 		#health = max(health - 1, 0)  # Ensures it doesn't go below 0
